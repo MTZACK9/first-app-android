422e422d1ec6503501f06b61b7afe43ab56c11e8
Zakaria MAIT <ziko.mait14@gmail.com>
2023-05-13 17:10:21 +0100

    First App

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextAmount = findViewById(R.id.editTextAmount);
+        Button buttonCompute = findViewById(R.id.buttonCompute);
+        TextView textViewResults = findViewById(R.id.textViewResults);
+        ListView listViewResults = findViewById(R.id.listViewResults);
+        List<String> data = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        listViewResults.setAdapter(adapter);
+        buttonCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double amount = Double.parseDouble(editTextAmount.getText().toString());
+                double result = amount * 65;
+                textViewResults.setText(String.valueOf(result));
+                data.add(amount + " => " + result);
+                adapter.notifyDataSetChanged();
+                editTextAmount.setText("");
+            }
+        });
+
+    }
+}
